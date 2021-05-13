2d9d370aecf430c65e51393a1d28e8af613146f0
ejm5204 <ermcmull@microsoft.com>
2021-05-13 14:33:26 -0400

    Add files via upload

---
+++ Install-Applications.ps1
@@ -0,0 +1,46 @@
+# Software install Script
+#
+# Applications to install:
+#
+# DoD Teams
+# https://dod.teams.microsoft.us/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true
+# 
+# See comments on creating a custom setting to disable auto update message
+# https://community.notepad-plus-plus.org/post/38160
+
+
+#region Set logging 
+$logFile = "c:\temp\" + (get-date -format 'yyyyMMdd') + '_softwareinstall.log'
+function Write-Log {
+    Param($message)
+    Write-Output "$(get-date -format 'yyyyMMdd HH:mm:ss') $message" | Out-File -Encoding utf8 $logFile -Append
+}
+#endregion
+
+#region DoD_Teams
+try {
+    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\Teams_windows_x64.msi'
+    if (Test-Path "C:\Program Files (x86)\Teams Installer\Teams.exe") {
+        Write-Log "Teams has been installed"
+    }
+    else {
+        write-log "Error locating the Teams executable"
+    }
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error installing Teams: $ErrorMessage"
+}
+#endregion
+
+#region Sysprep Fix
+# Fix for first login delays due to Windows Module Installer
+try {
+    ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
+    write-log "Sysprep Mode:VM fix applied"
+}
+catch {
+    $ErrorMessage = $_.Exception.message
+    write-log "Error updating script: $ErrorMessage"
+}
+#endregion
