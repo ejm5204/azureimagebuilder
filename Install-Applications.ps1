9e5a165c4bbf64062790f4f555eea33306e38bba
ejm5204 <ermcmull@microsoft.com>
2021-05-14 08:16:27 -0400

    Update to line 22; included '/quiet' flag

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -19,7 +19,7 @@ function Write-Log {
 
 #region DoD_Teams
 try {
-    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\Teams_windows_x64.exe'
+    Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\Teams_windows_x64.exe', '/quiet'
     if (Test-Path "C:\Program Files (x86)\Teams Installer\Teams.exe") {
         Write-Log "Teams has been installed"
     }
