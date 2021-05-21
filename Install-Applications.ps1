ff64f0b1cd2ee303170f549bee9ec2fc3697d659
Eric McMullen <eric.mcmullen@microsoft.com>
2021-05-21 09:45:54 -0400

    rewritten to use sas

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -2,8 +2,9 @@
 #
 # Applications to install:
 #
-# DoD Teams
-# https://dod.teams.microsoft.us/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true
+# Enterprise Chrome
+# DoD Teams: https://dod.teams.microsoft.us/downloads/desktopurl?env=production&plat=windows&arch=x64&managedInstaller=true&download=true
+# O365 OPP
 # 
 # See comments on creating a custom setting to disable auto update message
 # https://community.notepad-plus-plus.org/post/38160
@@ -34,7 +35,7 @@ catch {
 #endregion
 
 #region DoD Teams
-<# try {
+try {
     Start-Process -filepath msiexec.exe -Wait -ErrorAction Stop -ArgumentList '/i', 'c:\temp\Teams_windows_x64.msi', '/quiet'
     & "C:\Program Files (x86)\Teams Installer\Teams.exe"
     if (Test-Path "C:\Program Files (x86)\Teams Installer\Teams.exe") {
@@ -47,7 +48,25 @@ catch {
 catch {
     $ErrorMessage = $_.Exception.message
     write-log "Error installing DoD Teams: $ErrorMessage"
-} #>
+}
+#endregion
+
+#region O365 OPP
+try {
+  & c:/ODT/ODT_tool.exe /quiet /extract:c:/ODT
+  & c:/ODT/setup.exe /download c:/ODT/installOfficeProPlus64.xml
+  & c:/ODT/setup.exe /configure c:/ODT/installOfficeProPlus64.xml
+  if (Test-Path "C:\Program Files\Microsoft Office") {
+      Write-Log "Office has been installed"
+  }
+  else {
+      write-log "Error Office executable"
+  }
+}
+catch {
+  $ErrorMessage = $_.Exception.message
+  write-log "Error installing Office: $ErrorMessage"
+}
 #endregion
 
 #region Sysprep Fix
