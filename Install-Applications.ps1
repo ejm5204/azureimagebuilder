3741949be75200767480f6b1390ad4cfde7e49b9
Eric McMullen <eric.mcmullen@microsoft.com>
2021-05-28 12:20:40 -0400

    removed ODT download command (OPP working)

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -43,7 +43,7 @@ catch {
 
 try {
   & c:/ODT/ODT_tool.exe /quiet /extract:c:/ODT
-  Start-Process -filepath "setup.exe" -WorkingDirectory "c:\ODT" -ArgumentList '/download', 'c:/ODT/installOfficeProPlus64.xml' -Wait -ErrorAction Stop
+  #Start-Process -filepath "setup.exe" -WorkingDirectory "c:\ODT" -ArgumentList '/download', 'c:/ODT/installOfficeProPlus64.xml' -Wait -ErrorAction Stop
   Start-Process -filepath "setup.exe" -WorkingDirectory "c:\ODT" -ArgumentList '/configure', 'c:/ODT/installOfficeProPlus64.xml' -Wait -ErrorAction Stop
   if (Test-Path "C:\Program Files\Microsoft Office") {
       Write-Log "Office has been installed"
