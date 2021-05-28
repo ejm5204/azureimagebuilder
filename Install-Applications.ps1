8a3375ae7e818ddb916fea6adbe8bc2552021970
Eric McMullen <eric.mcmullen@microsoft.com>
2021-05-28 07:07:21 -0400

    install_office.bat

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -44,8 +44,9 @@ catch {
 
 try {
   & c:/ODT/ODT_tool.exe /quiet /extract:c:/ODT
-  Start-Process -filepath "c:\ODT\setup.exe" -ArgumentList '/download', 'c:\ODT\installOfficeProPlus64.xml' -Wait -ErrorAction Stop
-  Start-Process -filepath "c:\ODT\setup.exe" -ArgumentList '/configure', 'c:\ODT\installOfficeProPlus64.xml' -Wait -ErrorAction Stop
+  & c:/ODT/install_office.bat
+  #Start-Process -filepath "c:\ODT\setup.exe" -ArgumentList '/download', 'c:\ODT\installOfficeProPlus64.xml' -Wait -ErrorAction Stop
+  #Start-Process -filepath "c:\ODT\setup.exe" -ArgumentList '/configure', 'c:\ODT\installOfficeProPlus64.xml' -Wait -ErrorAction Stop
   if (Test-Path "C:\Program Files\Microsoft Office") {
       Write-Log "Office has been installed"
   }
