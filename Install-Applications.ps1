7c2f2e702a3fad6fcd192193cdbe39352f963cc8
Eric McMullen <eric.mcmullen@microsoft.com>
2021-07-21 15:49:29 -0400

    log edit

--- Install-Applications.ps1
+++ Install-Applications.ps1
@@ -102,6 +102,7 @@ catch {
 try {
     ((Get-Content -path C:\DeprovisioningScript.ps1 -Raw) -replace 'Sysprep.exe /oobe /generalize /quiet /quit', 'Sysprep.exe /oobe /generalize /quit /mode:vm' ) | Set-Content -Path C:\DeprovisioningScript.ps1
     write-log "Sysprep Mode:VM fix applied"
+    Write-Log "Log complete"
 }
 catch {
     $ErrorMessage = $_.Exception.message
